d7bbe8169b9eb729d8d518187fc3a9277ab8d0b0
Robert <robertjacobmyers@gmail.com>
2026-02-23 12:20:13 +0000

    added simple login page and class for demonstration + refactored to PasswordField

--- src/main/java/Controller/StartLogin.java
+++ src/main/java/Controller/StartLogin.java
@@ -4,18 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class StartLogin {
 
-    @FXML
-    Button loginButton;
-    @FXML
-    TextField usernameField;
-    @FXML
-    TextField passwordField;
-    @FXML
-    Label errorLabel;
+    @FXML Button loginButton;
+    @FXML TextField usernameField;
+    @FXML PasswordField passwordField;
+    @FXML Label errorLabel;
 
     public StartLogin(){
     }
