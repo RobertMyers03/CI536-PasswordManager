a545b20f1dce52e146ab7f2abe378b5fe7ad0b6d
Robert <robertjacobmyers@gmail.com>
2026-02-24 16:13:04 +0000

    Created SceneController.java which contains a method to switch, to dashboardView.fxml

--- src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
+++ src/main/java/com/example/passwordmanager/PasswordManagerApplication.java
@@ -2,18 +2,23 @@ package com.example.passwordmanager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class PasswordManagerApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PasswordManagerApplication.class.getResource("StartLoginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Password Manager");
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
+        Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Password Manager");
         stage.show();
     }
+
+
 }
