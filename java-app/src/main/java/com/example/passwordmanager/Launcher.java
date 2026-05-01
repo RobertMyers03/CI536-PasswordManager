b9ca2f6bdbfdf73351c383f4412af526646086aa
rm1798 <robertjacobmyers@gmail.com>
2026-05-01 14:31:41 +0100

    Main.py

--- java-app/src/main/java/com/example/passwordmanager/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.passwordmanager;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(PasswordManagerApplication.class, args);
-    }
-}
--- java-app/src/main/java/com/example/passwordmanager/security/CryptoUtils.java
+++
@@ -1,4 +0,0 @@
-package com.example.passwordmanager.security;
-
-public class CryptoUtils{
-}
